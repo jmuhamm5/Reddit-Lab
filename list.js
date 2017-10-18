b0e8b6794f597939cf1f99641819e8bf9f47f377
jmuhamm5 <apple_life15@yahoo.com>
2017-10-18 11:31:57 -0500

    New Commit

--- list.js
+++ list.js
@@ -1,4 +1,71 @@
 $(document).ready(function(){
+
+    $.ajax({
+      type: 'GET',
+      url: 'https://www.reddit.com/r/worldnews.json',
+      data: { get_param: 'value' },
+      dataType: 'json',
+      success: function (data) {
+          $.each(data, function(index, element) {
+              $('body').append($('<div>', {
+                  text: element.name
+              }));
+          });
+      }
+  }).then(function(success) {
+      var posts = configureResponse(success);
+
+      posts.forEach(function(post) {
+          var container = document.createElement('div');
+          var heading = document.createElement('h2');
+          var a = document.createElement('a');
+          var image = document.createElement('img');
+
+          heading.innerText = post.title;
+          image.setAttribute('src', formatSrc(post));
+          a.setAttribute('href', 'single.html?url=' + post.permalink);
+          a.appendChild(heading);
+          container.appendChild(a);
+          container.appendChild(image);
+          document.body.appendChild(container);
+      });
+
+  });
+
+});
+
+function configureResponse(data) {
+    var children = data.data.children;
+
+    return children.map(function(child, i) {
+        var post = {};
+        post.thumbnail = child.data.thumbnail;
+        post.title = child.data.title;
+        post.url = child.data.url;
+        post.permalink = child.data.permalink;
+
+        return post;
+    });
+}
+
+function formatSrc(post) {
+    if (/\.(gif|.gifv|jpg|jpeg|tiff|png)$/i.test(post.url)) {
+        if (post.url.indexOf('.gifv') != -1) {
+            return post.url.replace('.gifv', '.gif');
+        }
+
+        return post.url;
+    }
+
+    return post.thumbnail;
+}
+
+
+
+
+
+
+  /*
   $.ajax({
     method: "GET",
     url: "https://www.reddit.com/r/worldnews.json",
@@ -23,14 +90,7 @@ $(document).ready(function(){
       });
 
   });
-});
-
-
-
-
-
-
-
+  */
 
 
 
--- single.js
+++ single.js
@@ -1,10 +1,66 @@
 $(document).ready(function(){
-  $.ajax({
-    method: "GET",
-    url: "https://www.reddit.com/r/worldnews.json",
-    contentType: "application/json",
-    data: JSON.parse()
-  }).then();
+    var url = location.search.substring(1).split('=')[1];
+    $.ajax({
+      type: 'GET',
+      url: 'https://www.reddit.com' + url + '.json',
+      data: { get_param: 'value' },
+      dataType: 'json',
+      success: function (data) {
+          $.each(data, function(index, element) {
+              $('body').append($('<div>', {
+                  text: element.name
+              }));
+          });
+      }
+  }).then(function(success) {
+      var data = success[0];
+      var post = configureResponse(data);
+
+      var container = document.createElement('div');
+      var heading = document.createElement('h2');
+      var image = document.createElement('img');
+
+      heading.innerText = post.title;
+      image.setAttribute('src', formatSrc(post));
+      container.appendChild(heading);
+      container.appendChild(image);
+      document.body.appendChild(container);
+  });
+
+  function configureResponse(data) {
+      var children = data.data.children;
+
+      var result = children.map(function(child, i) {
+          console.log(child);
+          var post = {};
+          post.thumbnail = child.data.thumbnail;
+          post.title = child.data.title;
+          post.url = child.data.url;
+          post.permalink = child.data.permalink;
+
+          return post;
+      });
+
+      if (result.length === 1) {
+          return result[0];
+      }
+
+      return result;
+  }
+
+  function formatSrc(post) {
+      if (/\.(gif|.gifv|jpg|jpeg|tiff|png)$/i.test(post.url)) {
+          if (post.url.indexOf('.gifv') != -1) {
+              return post.url.replace('.gifv', '.gif');
+          }
+
+          return post.url;
+      }
+
+      return post.thumbnail;
+  }
+
+  );
 });
 
 
